406a1d1605a88cb8f576a2f9cbb91252206193c5
Ryan Gloff <ryan@ryangloff.com>
2025-03-30 18:47:40 -0400

    Fixed some issues in InsertParameterReadingComponent.vue

--- web-app/src/components/InsertParameterReadingComponent.vue
+++ web-app/src/components/InsertParameterReadingComponent.vue
@@ -1,32 +1,32 @@
 <script setup>
-  import EntitySelector from './EntitySelector.vue';
-  import fetchTanks from '../lib/fetchTanks';
-  import fetchParameters from '../lib/fetchParameters';
-  import postParameterReading from '../lib/postParameterReading';
+import EntitySelector from './EntitySelector.vue';
+import fetchTanks from '../lib/fetchTanks';
+import fetchParameters from '../lib/fetchParameters';
+import postParameterReading from '../lib/postParameterReading';
 
-  import { ref } from 'vue';
+import { ref } from 'vue';
 
-  const parameterReading = ref({
-    tankId: null,
-    parameterId: null,
-    value: null
-  });
+const parameterReading = ref({
+  tankId: null,
+  parameterId: null,
+  value: null
+});
 
-  const tank = ref(null);
-  const parameter = ref(null);
-  const value = ref(null);
+const tank = ref(null);
+const parameter = ref(null);
+const value = ref(null);
 
-  const sendRequest = async function() {
-    const parameterReading = {
-      tankId: tank.value.id,
-      parameterId: parameter.value.id,
-      value: value.value,
-      time: new Date(),
-      showInDashboard: true
-    };
-    console.log(`Storing new ParameterReading: ${JSON.stringify(parameterReading)}`);
-    await postParameterReading(parameterReading);
+const sendRequest = async function () {
+  const parameterReading = {
+    tankId: tank.value.id,
+    parameterId: parameter.value.id,
+    value: value.value,
+    time: new Date(),
+    showInDashboard: true
   };
+  console.log(`Storing new ParameterReading: ${JSON.stringify(parameterReading)}`);
+  await postParameterReading(parameterReading);
+};
 </script>
 
 <template>
@@ -34,11 +34,11 @@
     <div class="insert-parameter-reading">
       <h2>Create ParameterReading</h2>
       <div>
-        <EntitySelector entityName="Tank" :fetchFn="fetchTanks" entityNameField="name" v-model="tank"/>
-        <EntitySelector entityName="Parameter" :fetchFn="fetchParameters" entityNameField="name" v-model="parameter"/>
+        <EntitySelector entityName="Tank" :fetchFn="fetchTanks" entityNameField="name" v-model="tank" />
+        <EntitySelector entityName="Parameter" :fetchFn="fetchParameters" entityNameField="name" v-model="parameter" />
         <div class="field-entry">
           <label>Value: </label>
-          <input type="number" v-model="value"/>
+          <input type="number" v-model="value" />
         </div>
         <button class="sumbit" @click="sendRequest">Create</button>
       </div>
@@ -56,7 +56,7 @@
   grid-template-rows: auto 1fr;
 }
 
-.insert-parameter-reading > div {
+.insert-parameter-reading>div {
   display: grid;
   grid-template-rows: repeat(1fr, 4);
 }
@@ -65,7 +65,8 @@
   display: grid;
   grid-template-columns: 1fr 1fr;
 }
-.field-entry > input {
+
+.field-entry>input {
   padding: 5px;
 }
 </style>
